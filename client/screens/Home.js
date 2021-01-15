1b121787edd9dc7efa3a62c265cb5c28764aace1
oppabgnamdz <oppabgnamdz@gmail.com>
2021-01-15 18:29:17 +0700

    click image user save image to MediaLibrary

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,17 +1,42 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, TouchableOpacity, Alert } from 'react-native';
 import io from 'socket.io-client'
 import { GiftedChat, Send } from 'react-native-gifted-chat'
 import Spinner from 'react-native-loading-spinner-overlay';
 import SERVER from '../utils/Server';
 import ToggleSwitch from 'toggle-switch-react-native'
+import Modal from 'react-native-modal';
+
+import * as FileSystem from 'expo-file-system'
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function Home({ navigation, route }) {
     const { name, room, account, avatar, time } = route.params;
     const [receiveMessage, setReceiveMessage] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [toggle, setToggle] = useState(false);
+    const [isModalVisible, setModalVisible] = useState(false)
+    const [showImage, setShowImage] = useState('')
     const socket = useRef(null);
+    const saveFile = async (fileUri) => {
+        const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
+        if (status === "granted") {
+            const asset = await MediaLibrary.createAssetAsync(fileUri)
+            await MediaLibrary.createAlbumAsync("Download", asset)
+            Alert.alert("Save Image Successfully")
+        }
+    }
+    const downloadImage = () => {
+        let fileUri = FileSystem.documentDirectory + "robo.jpg";
+        FileSystem.downloadAsync(showImage, fileUri)
+            .then(({ uri }) => {
+                saveFile(uri);
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
     useEffect(() => {
         fetch(`${SERVER}${room}`)
             .then(res => res.json())
@@ -68,8 +93,13 @@ export default function Home({ navigation, route }) {
                 placeholder='Send something'
                 onSend={messages => onSend(messages)}
                 isAnimated={true}
+
                 user={{
                     _id: time,
+                }}
+                onPressAvatar={(user) => {
+                    setShowImage(user.avatar)
+                    setModalVisible(true)
                 }}
                 scrollToBottom={true}
                 renderSend={(props) => {
@@ -89,6 +119,33 @@ export default function Home({ navigation, route }) {
                     );
                 }}
             />
+            <Modal
+                style={{
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+                animationIn='slideInUp'
+                coverScreen={true}
+                animationInTiming={2000}
+                backdropOpacity={0.5}
+                onBackdropPress={() => setModalVisible(false)}
+                onSwipeComplete={() => setModalVisible(false)}
+                onBackButtonPress={() => {
+                    setModalVisible(false)
+                }}
+                isVisible={isModalVisible}>
+                <TouchableOpacity
+                    onPress={downloadImage}
+
+                >
+                    <Image
+                        source={{ uri: showImage }}
+                        style={{ width: 120, height: 120 }}
+                    />
+                </TouchableOpacity>
+
+            </Modal>
         </View>
+
     );
 }
